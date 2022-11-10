a5ec72a8b0f3bd50c87ef02990fbbbc3635b905d
Rahul-Codes <75724657+Rahulnair1234@users.noreply.github.com>
2022-11-11 01:41:04 +0530

    updated sponsers slider

--- script.js
+++ script.js
@@ -9,6 +9,33 @@ hamburger.addEventListener("click", () => {
     navMenu.classList.remove("active");
   }))
 
+  function maxWidthFunction(width) {
+    if (width.matches) { 
+      return true
+    } else {
+     return false
+    }
+  }
   
+  var width = window.matchMedia("(max-width: 850px)")
+  let forMobile = maxWidthFunction(width) 
+  width.addListener(maxWidthFunction)
   
-  
+  let mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if ((document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) && forMobile==true) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
